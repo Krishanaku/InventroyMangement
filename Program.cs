7d2f4c57eb74861e7cbd17004a5a4966ce3bdb4e
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-21 11:51:16 +0530

    Add : Display Diff Properties Data from Json

--- Program.cs
+++ Program.cs
@@ -9,12 +9,43 @@
             FetchInventory fetchInventory = new FetchInventory();
 
             InventoryDetails data = fetchInventory.Read(path);
+            Console.WriteLine("Type of Rice");
+            Console.WriteLine("*********************************************");
 
             for (int i = 0; i<data.typeofRice.Count; i++)
             {
                 Console.WriteLine(data.typeofRice[i].name);
                 Console.WriteLine(data.typeofRice[i].weight);
                 Console.WriteLine(data.typeofRice[i].prize);
+
+                Console.WriteLine("************************************************");
+
+            }
+
+
+            Console.WriteLine("Type of Pulses");
+            Console.WriteLine("*********************************************");
+
+            for (int i = 0; i < data.typeofPulses.Count; i++)
+            {
+                Console.WriteLine(data.typeofPulses[i].name);
+                Console.WriteLine(data.typeofPulses[i].weight);
+                Console.WriteLine(data.typeofPulses[i].prize);
+
+                Console.WriteLine("************************************************");
+
+            }
+
+            Console.WriteLine("Type of Wheat");
+            Console.WriteLine("*********************************************");
+
+            for (int i = 0; i < data.typeofWheat.Count; i++)
+            {
+                Console.WriteLine(data.typeofWheat[i].name);
+                Console.WriteLine(data.typeofWheat[i].weight);
+                Console.WriteLine(data.typeofWheat[i].prize);
+
+                Console.WriteLine("************************************************");
 
             }
 
