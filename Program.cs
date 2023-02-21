a62cc6ab3753ba5d4d5d4e7c19151a87c830dfed
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-21 11:23:43 +0530

    Add : Display data

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InventoryManagement
+{
+    class Program
+
+    {
+        public static void Main(string[] args )
+        {
+            string path = "E:\\Bridgelabz_C_Sharp\\InventoryManagement\\InventoryManagement\\Inventory.json";
+            FetchInventory fetchInventory = new FetchInventory();
+
+            InventoryDetails data = fetchInventory.Read(path);
+
+
+            Console.WriteLine(data.typeofRice.name);
+            Console.WriteLine(data.typeofRice.weight);
+            Console.WriteLine(data.typeofRice.prize);
+
+
+        }
+
+    }
+}
