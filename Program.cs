5d58f88438033c4c08e58fd26d9160af347f2838
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-21 12:14:22 +0530

    Add :  Calculate Diff Properties Data from json

--- Program.cs
+++ Program.cs
@@ -17,6 +17,9 @@
                 Console.WriteLine(data.typeofRice[i].name);
                 Console.WriteLine(data.typeofRice[i].weight);
                 Console.WriteLine(data.typeofRice[i].prize);
+                int prize = data.typeofRice[i].weight * data.typeofRice[i].prize;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                    data.typeofRice[i].name,data.typeofRice[i].weight, prize);
 
                 Console.WriteLine("************************************************");
 
@@ -31,6 +34,10 @@
                 Console.WriteLine(data.typeofPulses[i].name);
                 Console.WriteLine(data.typeofPulses[i].weight);
                 Console.WriteLine(data.typeofPulses[i].prize);
+                int prize = data.typeofPulses[i].weight * data.typeofPulses[i].prize;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                    data.typeofPulses[i].name, data.typeofPulses[i].weight, prize);
+
 
                 Console.WriteLine("************************************************");
 
@@ -44,6 +51,9 @@
                 Console.WriteLine(data.typeofWheat[i].name);
                 Console.WriteLine(data.typeofWheat[i].weight);
                 Console.WriteLine(data.typeofWheat[i].prize);
+                int prize = data.typeofWheat[i].weight * data.typeofWheat[i].prize;
+                Console.WriteLine("The prize of {0} for {1}kg is {2}",
+                    data.typeofWheat[i].name, data.typeofWheat[i].weight, prize);
 
                 Console.WriteLine("************************************************");
 
