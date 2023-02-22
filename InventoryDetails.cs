ff10d7407258748243c5a056ade7b8b8868e5b5b
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-22 13:43:50 +0530

    Add : get data from json

---
+++ InventoryDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfStock> typesofstock;
+
+    }
+    public class TypesOfStock
+    {
+        public int numberofshare;
+        public int shareprice;
+        public string? stockname;
+    }
+}
+
