77e0dfa592561057094d88a7852ee55140b46f61
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-22 13:37:59 +0530

    Add kk

--- Program.cs
+++ Program.cs
@@ -5,8 +5,25 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Inventory Management Program");
-            
+            Console.WriteLine("Welcome to Inventory Management Program Stock Problem!!");
+
+            string path = "E:\\Bridgelabz_C_Sharp\\InventoryManagement\\InventoryManagement\\Inventory.json";
+            FetchInventory fetchInventory = new FetchInventory();
+
+            InventoryDetails data = fetchInventory.Read(path);
+
+
+            for (int i = 0; i < data.typesofstock.Count; i++)
+            {
+                Console.WriteLine(data.typesofstock[i].stockname);
+                Console.WriteLine(data.typesofstock[i].shareprice);
+                Console.WriteLine(data.typesofstock[i].numberofshare);
+                int total = data.typesofstock[i].numberofshare * data.typesofstock[i].shareprice;
+                Console.WriteLine("Total Stock Price : " + total);
+                Console.WriteLine("********************************************");
+            }
+        }
+           
 
     }
 }
